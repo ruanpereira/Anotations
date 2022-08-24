81ac3a8bbcca0bec0ac64d10003dfeeddaa0e5f0
RuanAlves <77154574+ruanpereira@users.noreply.github.com>
2022-08-24 19:38:01 -0300

    Add files via upload

---
+++ Scripts/Mov_Wallpaper.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+find /home/sacode02/Downloads -name "*wall*" -exec mv -t /usr/share/backgrounds {} + 
+echo "feito!!!"
---
+++ Scripts/Quem_chupa.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+read -p "Diga até 3 nomes ai que chupam mto, e coloque espaço entre cada nome: " nom1 nom2 nom3
+echo "$nom1 CHUPA MUUUUUUUITA PIKA" 
+echo "$nom2 NÃO TANTO QUANTO O 1, MAS AINDA É MTO, FILHO *FUNGADA*"
+echo "$nom3 AGR ESSE AQUI FAZ INVEJA AO BAÚ DO MINE DE TANTO QUE AGUENTA PIKA" 
+
+echo "feito"
